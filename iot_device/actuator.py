1dd2d3d22ed6a680ace4ff3456638bbe4a814860
werebecca <wrebecca@ethz.ch>
2020-11-07 23:03:37 +0100

    all actuators combined into one file

--- iot_device/actuator.py
+++ iot_device/actuator.py
@@ -14,6 +14,36 @@ class Actuator(IOT_Device):
         if retval == 0:
             self.initialized = True
 
+    def check_for_open_jobs(self):
+        pass
+
+
+class SmartDoorLock(Actuator):
+
+    def __init__(self, uniq_id):
+        super().__init__(uniq_id)
+        self.isDoorOpen = True  # State if door is open
+        self.openJobs = []
+
+    def update_status(self):
+        if self.openJobs:
+            self.isDoorOpen = self.openJobs[0]
+
+
+
+class SmartLamp(Actuator):
+    def __init__(self, uniq_id):
+        super().__init__(uniq_id)
+        self.intensity = 0  # Intensity of the lamp
+        self.openJobs = []
+
+    def update_status(self):
+        if self.openJobs:
+            self.intensity = self.openJobs[-1]
+
+
+
+
 
 if __name__ == '__main__':
     dummy_dev = Actuator(uniq_id='000000')
