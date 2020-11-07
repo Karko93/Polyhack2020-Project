dfb69c6e077806789948654ad347c86901012785
dwindey <dwindey@ethz.ch>
2020-11-07 12:55:20 +0100

    create fat websocket based on flask

--- websocket/server.py
+++ websocket/server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/hello_again')
+def hello_world_again():
+    return 'Hello again, World!'
+
 # @app.route('/', methods=['GET', 'POST'])
 # def index():
 #     if request.method == 'POST':
@@ -18,3 +22,6 @@ def hello_world():
 # def favicon():
 #     return send_from_directory(os.path.join(app.root_path, 'static'),
 #                     'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+    app.run(debug=True)
