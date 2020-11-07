52f37c5235542264383a132a31ab961f978b8667
Eric Bonvin <eric.bonvin@me.com>
2020-11-07 12:23:57 +0100

    Update websocket

--- websocket.py
+++ websocket.py
@@ -1,1 +1,17 @@
 import flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        return render_template('index.html',
+                               name=request.form['name'])
+    return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                    'favicon.ico', mimetype='image/vnd.microsoft.icon')
