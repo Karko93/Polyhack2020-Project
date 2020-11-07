9be12b1c82e04d1ad13cce7998c282d3f3a144f5
Eric Bonvin <eric.bonvin@me.com>
2020-11-08 00:05:00 +0100

    Add rules_generator to server

--- websocket/server.py
+++ websocket/server.py
@@ -55,5 +55,43 @@ def send_json():
     # stuff happens here that involves data to obtain a result
     return json.dumps(result)
 
+
+@app.route('/rules_generator', methods=['GET', 'POST'])
+def rules_generator():
+
+    # sensors_list and actuators_list should be read from iot_server
+    sensors_list = {'000001':'Temperature', '000002':'Humidity'}
+    conditions_list = ['>','<', '=']
+    actuators_list = {'000001':'Heater', '000002':'DoorLock'}
+
+    # stuff happens here that involves data to obtain a result
+    if request.method == 'POST':
+        if 'Submit Rule' in request.form:
+            # Submit Rule
+            return render_template('rules_generator.html',
+                                   sensors_list=sensors_list,
+                                   conditions_list=conditions_list,
+                                   actuators_list=actuators_list,
+                                   )
+        required_inputs = ['sensor_id', 'condition', 'threshold', 'actuator_id', 'actuator_action']
+        request_ready = all(key in request.form for key in required_inputs)
+        return render_template('rules_generator.html',
+                               sensors_list=sensors_list,
+                               selected_sensor=request.form['sensor_id'] if 'sensor_id' in request.form else None,
+                               conditions_list=conditions_list,
+                               selected_condition=request.form['condition'] if 'condition' in request.form else None,
+                               threshold=request.form['threshold'] if 'threshold' in request.form else None,
+                               actuators_list=actuators_list,
+                               selected_actuator=request.form['actuator_id'] if 'actuator_id' in request.form else None,
+                               actuator_actions=['Do something', 'Do something else'] if 'actuator_id' in request.form else [],
+                               selected_action=request.form['actuator_action'] if 'actuator_action' in request.form else None,
+                               request_ready=request_ready
+                              )
+    return render_template('rules_generator.html',
+                           sensors_list=sensors_list,
+                           conditions_list=conditions_list,
+                           actuators_list=actuators_list,
+                          )
+
 if __name__ == '__main__':
     app.run()
