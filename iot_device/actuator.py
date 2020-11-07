1f9cff81a4349d84ec324e3f2f0cd9ddd83506b2
werebecca <wrebecca@ethz.ch>
2020-11-07 23:55:55 +0100

    send_to_server included

--- iot_device/actuator.py
+++ iot_device/actuator.py
@@ -1,10 +1,14 @@
 from iot_device.iot_dev import IOT_Device
+from datetime import datetime
 
 
 class Actuator(IOT_Device):
     jobs = {}
+
     def __init__(self, uniq_id):
         super().__init__(uniq_id)
+        self.actuator_type = None
+        self.status = None
         self.init_at_host()
 
     def init_at_host(self):
@@ -12,19 +16,24 @@ class Actuator(IOT_Device):
         message = {'id': self.uniq_id, 'ancestors': self.ancestors, 'jobs': self.jobs}
         retval = self._post('actuator_com', message)
 
-    def check_for_open_jobs(self):
-        pass
+    def send_to_server(self):
+        tsmp = datetime.timestamp(datetime.now())
+        self._data = {self.actuator_type: self.status, 'timestamp': tsmp}
+
 
 
 class SmartDoorLock(Actuator):
     def __init__(self, uniq_id):
         super().__init__(uniq_id)
+        self.actuator_type = "doorlock"
         self.isDoorOpen = True  # State if door is open
         self.openJobs = {}
+        self.status = self.isDoorOpen
 
     def update_status(self):
         if self.openJobs:
             self.isDoorOpen = self.openJobs['openDoor']
+            self.status = self.isDoorOpen
             self.openJobs = {}
 
 
@@ -32,50 +41,62 @@ class SmartDoorLock(Actuator):
 class MotorPosition(Actuator):
     def __init__(self, uniq_id, position_x = 0, position_y = 0):
         super().__init__(uniq_id)
+        self.actuator_type = "motorposition"
         self.position_x = position_x # heating switched on
         self.position_y = position_y
+        self.status = [self.position_x, self.position_y]
         self.openJobs = {}
 
     def update_status(self):
         if self.openJobs:
             self.position_x = self.openJobs['position_x']
             self.position_y = self.openJobs['position_y']
+            self.status = [self.position_x, self.position_y]
             self.openJobs = {}
 
 
 class SmartLamp(Actuator):
     def __init__(self, uniq_id):
         super().__init__(uniq_id)
+        self.actuator_type = "smartlamp"
         self.intensity = 0  # Intensity of the lamp
+        self.status = self.intensity
         self.openJobs = {}
 
     # Check if there are open jobs and update the intensity according to the las value
     def update_status(self):
         if self.openJobs:
             self.intensity = self.openJobs['intensity']
+            self.status = self.intensity
             self.openJobs = {}
 
 
 class Heating(Actuator):
     def __init__(self, uniq_id):
         super().__init__(uniq_id)
+        self.actuator_type = "heating"
         self.heating_on = 0  # heating switched on
+        self.status = self.heating_on
         self.openJobs = {}
 
     def update_status(self):
         if self.openJobs:
             self.heating_on = self.openJobs['heating_on']
+            self.status = self.heating_on
             self.openJobs = {}
 
 class Sprinkler(Actuator):
     def __init__(self, uniq_id):
         super().__init__(uniq_id)
+        self.actuator_type = "sprinkler"
         self.water_running = 0  # sprinkler switched on
+        self.status = self.water_running
         self.openJobs = {}
 
     def update_status(self):
         if self.openJobs:
             self.water_running = self.openJobs['water_running']
+            self.status = self.water_running
             self.openJobs = {}
 
 if __name__ == '__main__':
