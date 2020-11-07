5161e3344f1f8d7553a3dc83c84a1a7c0691a6a9
Karko93 <MarcoKetzel6@yahoo.de>
2020-11-07 15:59:11 +0100

    Changed names

--- iot_device/smart_lamp.py
+++ iot_device/smart_lamp.py
@@ -9,9 +9,9 @@ from actuator import Actuator
 class SmartLamp(Actuator):
 
     def __init__(self, jsonFile):
-        self.serial = jsonFile['serial'] #instantiated Serialnumber
         self.intensity = 0 # Intensity of the lamp
 
     def SetIntensity(self, value):
         self.intensity = value
         #Callback to Server is needed
+        #Callback to Environment needed
