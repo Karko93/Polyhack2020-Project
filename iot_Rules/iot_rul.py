aa631add08d25ed693c241d5add628e162b94862
werebecca <wrebecca@ethz.ch>
2020-11-07 21:25:18 +0100

    update iot_rules

--- iot_Rules/iot_rul.py
+++ iot_Rules/iot_rul.py
@@ -1,27 +1,28 @@
 import json
 import requests
+from websocket.server import iot_server
+
 
 class IOT_Rules:
 
     def __init__(self, uniq_id):
         self.uniq_id = uniq_id
-        self.sensors = []
-        self.actuators = []
+        self.sensor_ids = []
+        self.actuator_ids = []
+        self.data = []
 
-    def _post(self, address, message):
-        """Wrapper function for POST interaction with the server."""
-        s = json.dumps(message)
-        host = self.hostname + address
-        try:
-            return int(requests.post(host, json=s).content)
-        except:
-            return 1
 
     def collect_sensor_data(self):
+        for sensor_id in self.sensor_ids:
+            if sensor_id in iot_server.sensors:
+                self.data.append(iot_server.sensors[sensor_id].data.iloc[-1])
+
+    def rule_decision(self):
         pass
 
     def send_actuator_jobs(self):
-        pass
+        decisions = rule_decision(self)
+        return (self.actuator_ids, decisions)
 
 if __name__ == '__main__':
     dummy_rul = IOT_Rules(uniq_id='000000')
