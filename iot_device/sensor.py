666cc0d44db1f90cb74987361f5d71cb6750cd42
AndreiMilitaru <45876588+AndreiMilitaru@users.noreply.github.com>
2020-11-07 21:38:54 +0100

    Update sensor.py

--- iot_device/sensor.py
+++ iot_device/sensor.py
@@ -1,4 +1,5 @@
 from iot_device.iot_dev import IOT_Device
+from datetime import datetime
 
 
 class Sensor(IOT_Device):
@@ -8,6 +9,67 @@ class Sensor(IOT_Device):
     def send_to_server(self, data):
         message = {'id': self.uniq_id, 'ancestors': self.ancestors, 'data':data}
         retval = self._post('sensor_com', message)
+
+
+class PhysicalSensor(Sensor):
+
+    sensor_type = None
+
+    def __init__(self, uniq_id, env):
+        super().__init__(uniq_id)
+        self._data = None
+        self.data_point = None
+        self.env = env
+        self.env.add_sensor(self)
+
+    def read_data(self):
+        self.data_point = self.env.read_value(self.uniq_id)
+        return self
+
+    def parse_data(self):
+        tsmp = datetime.timestamp(datetime.now())
+        self._data = {self.sensor_type: self.data_point, 'timestamp' : tsmp}
+
+    @property
+    def data(self):
+        self.read_data()
+        self.parse_data()
+        return self._data
+
+
+class TemperatureSensor(PhysicalSensor):
+
+    def __init__(self, uniq_id, env):
+        self.sensor_type = 'temperature'
+        super().__init__(uniq_id, env)
+
+
+class HumiditySensor(PhysicalSensor):
+
+    def __init__(self, uniq_id, env):
+        self.sensor_type = 'humidity'
+        super().__init__(uniq_id, env)
+
+
+class MotionSensor(PhysicalSensor):
+
+    def __init__(self, uniq_id, env):
+        self.sensor_type = 'motion_sensor'
+        super().__init__(uniq_id, env)
+
+
+class NoiseSensor(PhysicalSensor):
+
+    def __init__(self, uniq_id, env):
+        self.sensor_type = 'noise_detector'
+        super().__init__(uniq_id, env)
+
+
+class BrightnessSensor(PhysicalSensor):
+
+    def __init__(self, uniq_id, env):
+        self.sensor_type = 'brightness_sensor'
+        super().__init__(uniq_id, env)
 
 
 if __name__ == '__main__':
