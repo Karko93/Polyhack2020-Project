a9dbb35cee9e5932234c411aff2c26634d2a1e76
dwindey <dwindey@ethz.ch>
2020-11-07 13:54:04 +0100

    created base classes for sensors/actuators

--- iot_device/sensor.py
+++ iot_device/sensor.py
@@ -1,2 +1,8 @@
-class Sensor:
-    pass
+from iot_device.iot_dev import IOT_Device
+
+class Sensor(IOT_Device):
+    def __init__(self):
+        pass
+
+    def send_to_server(self, data):
+        pass
