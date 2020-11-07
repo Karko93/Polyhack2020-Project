4ba5779dd9a55c8e2d10e9af3ed61a4943688bfb
Karko93 <MarcoKetzel6@yahoo.de>
2020-11-07 22:26:21 +0100

    Progress

--- IoT_Rules/smart_flood_light_rule.py
+++ IoT_Rules/smart_flood_light_rule.py
@@ -5,7 +5,7 @@ class SmartFloodLight(IOT_Rules):
     def rule_decision(self):
         
         if self.data[0] == True and self.data[1]==True:
-            return [False for i in range(len(self.actuator_ids))]
+            return [True for i in range(len(self.actuator_ids))]
         else:
             return [False for i in range(len(self.actuator_ids))]
         
