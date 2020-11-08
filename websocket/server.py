9ba6b16e81ea18f576a268312651dde801c255b1
Eric Bonvin <eric.bonvin@me.com>
2020-11-08 09:32:49 +0100

    Rules generator works

--- websocket/server.py
+++ websocket/server.py
@@ -3,6 +3,7 @@ import requests
 import json
 from websocket.backend import IOT_Server
 from websocket.helper import html_table, BackgroundWorker
+from iot_rules.iot_rul import IOT_Rules
 import threading
 
 # suppress output to console
@@ -66,34 +67,57 @@ def send_json():
 def rules_generator():
 
     # sensors_list and actuators_list should be read from iot_server
-    sensors_list = {'000001':'Temperature', '000002':'Humidity'}
+    devices = iot_server.devices
+    sensors_list = {k:'{} ({})'.format(type(v).__name__ , k)
+                    for k, v in devices.items() if v.kind=='Sensor'}
+    # sensors_list = {'000001':'Temperature', '000002':'Humidity'}
     conditions_list = ['>', '<', '=', '!=']
-    actuators_list = {'000001':'Heater', '000002':'DoorLock'}
+    actuators_list = {k:'{} ({})'.format(type(v).__name__ , k)
+                    for k, v in devices.items() if v.kind=='Actuator'}
 
     # stuff happens here that involves data to obtain a result
     if request.method == 'POST':
         if 'Submit Rule' in request.form:
             # Submit Rule
+            uniq_id = 233893
+            rule = IOT_Rules(uniq_id,
+                             sensor_id_list=[request.form['sensor_id']],
+                             sensor_reading=[request.form['sensor_reading']],
+                             actuator_id_list=[request.form['actuator_id']],
+                             actuator_output=[request.form['actuator_action']],
+                             comparisons=[request.form['condition']],
+                             thresholds=[request.form['threshold']],
+                             actuator_value_True=[request.form['true_value']],
+                             actuator_value_False=[request.form['false_value']] if request.form['false_value'] else None,
+                             requirement='all')
+            iot_server.rules.append(rule)
+
             return render_template('rules_generator.html',
                                    sensors_list=sensors_list,
                                    conditions_list=conditions_list,
                                    actuators_list=actuators_list,
                                    )
-        required_inputs = ['sensor_id', 'sensor_reading', 'condition', 'threshold', 'actuator_id', 'actuator_action']
-        request_ready = all(key in request.form for key in required_inputs)
+        required_inputs = ['sensor_id', 'sensor_reading', 'condition', 'threshold', 'actuator_id', 'actuator_action',
+                           'true_value', 'false_value']
+        request_ready = all(key in request.form for key in required_inputs) and request.form['threshold'] and request.form['true_value']
+        sensor_readings = devices[request.form['sensor_id']].data.columns if 'sensor_id' in request.form else []
+        actuator_actions = devices[request.form['actuator_id']].data.columns if 'actuator_id' in request.form else []
+
         return render_template('rules_generator.html',
                                sensors_list=sensors_list,
                                selected_sensor=request.form['sensor_id'] if 'sensor_id' in request.form else None,
-                               sensor_readings=['Reading A', 'Reading B'] if 'sensor_id' in request.form else [],
+                               sensor_readings=sensor_readings,
                                selected_reading=request.form['sensor_reading'] if 'sensor_reading' in request.form else None,
                                conditions_list=conditions_list,
                                selected_condition=request.form['condition'] if 'condition' in request.form else None,
                                threshold=request.form['threshold'] if 'threshold' in request.form else None,
                                actuators_list=actuators_list,
                                selected_actuator=request.form['actuator_id'] if 'actuator_id' in request.form else None,
-                               actuator_actions=['Do something', 'Do something else'] if 'actuator_id' in request.form else [],
+                               actuator_actions=actuator_actions,
                                selected_action=request.form['actuator_action'] if 'actuator_action' in request.form else None,
-                               request_ready=request_ready
+                               request_ready=request_ready,
+                               true_value=request.form['true_value'] if 'true_value' in request.form else None,
+                               false_value=request.form['false_value'] if 'false_value' in request.form else None,
                               )
     return render_template('rules_generator.html',
                            sensors_list=sensors_list,
