003f8182e1a5e575ab30b0cd7b57c39316513415
dwindey <dwindey@ethz.ch>
2020-11-08 10:06:58 +0100

    Revert "added sensor parser, not sure if it works?"
    This reverts commit d70dbe36322db78cb0a7072d4380ec4acb33908e.
    
    # Conflicts:
    #	Documents/initialisation.json
    #	iot_device/environment.py
    #	iot_device/sensor.py
    #	test_sensor.py

--- iot_device/environment.py
+++ iot_device/environment.py
@@ -1,14 +1,12 @@
 import numpy as np
-from threading import Thread
-from time import sleep
 
 def initialize(sensor_type):
     if sensor_type == 'noise_detector' or sensor_type == 'motion':
         return np.random.rand() > 0.5
     elif sensor_type == 'temperature':
-        return 25.0 + np.random.normal(scale=2)
+        return 25.0 + np.random.normal(scale=10)
     elif sensor_type == 'humidity':
-        return 50.0 + np.random.normal(scale=20)
+        return 50.0 + np.random.normal(scale=10)
     elif sensor_type == 'brightness':
         return 50.0 + np.random.normal(scale=10)
     elif sensor_type == 'proximity':
@@ -21,40 +19,10 @@ def initialize(sensor_type):
         return [np.random.rand() for i in range(100)]
     else:
         raise ValueError('Sensor type {} unknown'.format(sensor_type))
-        
-### influence of actuators on sensor in same position 
-def change_sensor_value(actuator_type,actuator_data,sensor_type,sensor_value):
-    if actuator_type == 'light':
-        if sensor_type == 'brightness':
-            return sensor_value +actuator_data['intensity']*30 
-    
-    return sensor_value
-        
-        
-
-
-
-class EnvironmentManager(Thread):
-
-    def __init__(self, env,actuators):
-        Thread.__init__(self)
-        self.env = env
-        self.daemon = True
-        self.actuators = actuators
-
-    def run(self):
-        while True:
-            self.env.update_environment()
-            self.env.change_val(self.actuators)
-            sleep(0.1)
-
 
 class Environment():
 
     sensors = None # this will be in form {'id1':{'temperature', value, fixed'}}
-
-
-
 
     def __init__(self, sensors={}):
         """
@@ -62,22 +30,18 @@ class Environment():
         :param sensors: dictionary of sensor connected to it (id : type_of_sensor)
         """
         self.sensors = {}
-        for sensor, sensor_type,sensor_pos in sensors.items():
+        for sensor, sensor_type in sensors.items():
             self.sensors[sensor] = {'sensor_type': sensor_type,
                                     'value': initialize(sensor_type),
                                     'fixed' : False,
-                                    'position': sensor_pos
                                    }
 
     def add_sensor(self, sensor):
         id = sensor.uniq_id
         sensor_type = sensor.sensor_type
-        sensor_pos = sensor.position
         self.sensors[id] = {'sensor_type' : sensor_type,
                             'value' : initialize(sensor_type),
-                            'fixed' : False,
-                            'position': sensor_pos
-                            }
+                            'fixed' : False}
         return self
 
     def update_environment(self):
@@ -96,14 +60,10 @@ class Environment():
             raise Exception('Sensor ' + sensor + ' not present in the list.')
         return self
 
-    def change_val(self,actuators):
-        for actuator in actuators:
-            for (sensor, information) in self.sensors.items():
-                if actuator.position == self.sensors[sensor]['position'] and self.sensors[sensor]['fixed'] == False:
-                    self.sensors[sensor]['value'] = change_sensor_value(actuator.actuator_type,actuator.data, self.sensors[sensor]['sensor_type'], self.sensors[sensor]['value'])
-        #if sensor in self.sensors:
-            #self.sensors[sensor]['value'] = self.sensors[sensor]['value']
-            #self.sensors[sensor]['fixed'] = True
-        #else:
-            #raise Exception('Sensor ' + sensor + ' not present in the list.')
-        #return self
+    def change_val(self, sensor, new_value):
+        if sensor in self.sensors:
+            self.sensors[sensor]['value'] = new_value
+            self.sensors[sensor]['fixed'] = True
+        else:
+            raise Exception('Sensor ' + sensor + ' not present in the list.')
+        return self
--- test_sensor.py
+++ test_sensor.py
@@ -1,45 +1,57 @@
-from iot_device.environment import Environment, EnvironmentManager
+from iot_device.environment import Environment
 from iot_device.sensor import *
-import iot_device.actuator
+from threading import Thread
 from time import sleep
-#from antenna_actuator_sensor import Antenna
+from antenna_actuator_sensor import Antenna
 
 import json
 
-filename_sensors = 'Documents/initialisation_sensor.json'
-filename_actuators = 'Documents/initialisation_actuator.json'
 
-with open(filename_sensors) as initialisation_file:
-    list_of_sensors = json.load(initialisation_file)
-with open(filename_actuators) as initialisation_file:
-    list_of_actuators = json.load(initialisation_file)
+class EnvironmentManager(Thread):
 
-actuators = [getattr(iot_device.actuator, act['ancestor'])(act['serial'], act['position']) for act in list_of_actuators]
-sensors = []
-new_sensors = []
+    def __init__(self, env):
+        Thread.__init__(self)
+        self.env = env
+        self.daemon = True
+
+    def run(self):
+        while True:
+            self.env.update_environment()
+            sleep(0.1)
 
 env = Environment()
-sensor_parser = SensorInstantiator(env, sensors=sensors, new_sensors=new_sensors)
-sensor_parser.start()
+sensors = []
 
 counter = 0
 
-for new_element in list_of_sensors:
-    sensor_parser.add_sensor(new_element)
+for id in range(5):
+    sensors.append(TemperatureSensor(str(counter).zfill(6), env))
+    counter += 1
 
+for id in range(5):
+    sensors.append(HumiditySensor(str(counter).zfill(6), env))
+    counter += 1
+
+for id in range(5):
+    sensors.append(NoiseSensor(str(counter).zfill(6), env))
+    counter += 1
+
+for id in range(5):
+    sensors.append(MotionSensor(str(counter).zfill(6), env))
+    counter += 1
+
+for id in range(10):
+    sensors.append(BrightnessSensor(str(counter).zfill(6), env))
+    counter += 1
+    
+for id in range(1):
+    sensors.append(Antenna(str(counter).zfill(6), env))
+    counter += 1
 
 clock = EnvironmentManager(env)
 clock.start()
 
-sleep(0.5)
-
-
-while True:
-    for sensor in sensor_parser.sensors:
-        data = sensor.data
-        print(sensor.uniq_id, data)
-        sensor.send_to_server(data)
-    for actuator in actuators:
-        actuator.update_status()
-    sleep(1)
-
+for sensor in sensors:
+    data = sensor.data
+    print(sensor.uniq_id, data)
+    sensor.send_to_server(data)
