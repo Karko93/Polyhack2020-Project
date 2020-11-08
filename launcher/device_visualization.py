e6e21cc0240f07411e652b523ab92c2f2f29e1a3
Andrei Militaru <andreimi@ethz.ch>
2020-11-08 13:07:53 +0100

    added map plotting of devices

---
+++ launcher/device_visualization.py
@@ -0,0 +1,27 @@
+import json
+import folium
+
+
+# Reading the initialisation files
+filename_sensors = 'initialisation_sensor.json'
+filename_actuators = 'initialisation_actuator.json'
+with open(filename_sensors) as initialisation_file:
+    list_of_sensors = json.load(initialisation_file)
+with open(filename_actuators) as initialisation_file:
+    list_of_actuators = json.load(initialisation_file)
+
+m = folium.Map(location=[47.40667, 8.55], zoom_start=13)
+
+for sensor in list_of_sensors[:20]:
+    posx, posy = sensor['position']['x'], sensor['position']['y']
+    # print(posx, posy)
+
+    folium.Marker(location=[posx, posy], popup=sensor['serial'], icon=folium.Icon(color='blue', icon='')).add_to(m)
+
+for actuator in list_of_actuators[:20]:
+    posx, posy = actuator['position']['x'], actuator['position']['y']
+    # print(posx, posy)
+
+    folium.Marker(location=[posx, posy], popup=actuator['serial'], icon=folium.Icon(color='red', icon='')).add_to(m)
+
+m.save('map.html')
