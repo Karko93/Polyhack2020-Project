59c05a554cd785206caf11a30542e67d97fce83a
AndreiMilitaru <45876588+AndreiMilitaru@users.noreply.github.com>
2020-11-08 11:21:07 +0100

    Update create_json_initialisation.py

--- launcher/create_json_initialisation.py
+++ launcher/create_json_initialisation.py
@@ -6,13 +6,23 @@ number_of_sensors = 1000
 number_of_actuators = 1000
 
 
-filename_sensors = 'initialisation_sensors.json'
-filename_actuators = 'initialisation_actuators.json'
+filename_sensors = 'initialisation_sensor.json'
+filename_actuators = 'initialisation_actuator.json'
 
 sensor_types = ['temperature', 'humidity', 'brightness', 'proximity', 'noise',
                 'distance', 'motion', 'airquality', 'distance']
 
 actuator_types = ['SmartLamp', 'SmartDoorLock', 'MotorPosition', 'Heating', 'Sprinkler']
+
+sensor_kinds = []
+actuator_kinds = []
+
+for i in range(number_of_sensors):
+    sensor_kinds.append(sensor_types[i % len(sensor_types)])
+
+for i in range(number_of_actuators):
+    actuator_kinds.append(actuator_types[i % len(actuator_types)])
+
 
 
 counter_sensors = 0
@@ -20,33 +30,31 @@ counter_sensors = 0
 counter_actuators = 0
 
 
-def create_sensor():
+def create_sensor(sensor_type):
     global counter_sensors
     newid = counter_sensors
     counter_sensors += 1
-    sensor_type = random.choice(sensor_types)
     posx, posy = 30 + np.random.normal(scale=10), 10 + np.random.normal(10)
-    dic = {"serial" : str(newid).zfill(5), "type" : "sensor", "ancestor" : sensor_type,
+    dic = {"serial" : newid, "type" : "sensor", "ancestor" : sensor_type,
             "position" : {"x": posx, "y": posy}}
     return dic
 
 
-def create_actuator():
+def create_actuator(actuator_type):
     global counter_actuators
-    newid = counter_actuators + 1000
+    newid = counter_actuators + 10000
     counter_actuators += 1
-    actuator_type = random.choice(actuator_types)
     posx, posy = 30 + np.random.normal(scale=10), 10 + np.random.normal(10)
-    dic = {"serial" : str(newid).zfill(5), "type" : "Actuator", "ancestor" : actuator_type,
+    dic = {"serial" : newid, "type" : "Actuator", "ancestor" : actuator_type,
             "position" : {"x": posx, "y": posy}}
     return dic
 
 
-actuators = [create_actuator() for i in range(number_of_actuators)]
-sensors = [create_sensor() for i in range(number_of_sensors)]
+actuators = [create_actuator(actuator_kinds[i]) for i in range(number_of_actuators)]
+sensors = [create_sensor(sensor_kinds[i]) for i in range(number_of_sensors)]
 
 with open(filename_sensors, 'w') as f:
-    json.dump(sensors, f, indent=4)
+    json.dump(sensors, f)
 
 with open(filename_actuators, 'w') as f:
-    json.dump(actuators, f, indent=4)
+    json.dump(actuators, f)
