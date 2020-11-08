038705beb394a209766b9f0fa2162ffc5cbf8591
Andrei Militaru <andreimi@ethz.ch>
2020-11-08 08:33:01 +0100

    initialisation of sensors directly from file

--- test_sensor.py
+++ test_sensor.py
@@ -18,101 +18,10 @@ sensor_parser = SensorInstantiator(env, sensors=sensors, new_sensors=new_sensors
 sensor_parser.start()
 
 counter = 0
-"""
-list_of_sensors = [{
-  "serial": str(1).zfill(2),
-  "type": "sensor",
-  "ancestor": "brightness",
-  "position":{
-    "x": 47.410847,
-    "y":  8.537878}
-}, {
-  "serial": str(2).zfill(2),
-  "type": "sensor",
-  "ancestor": "temperature",
-  "position":{
-    "x": 47.410847,
-    "y":  8.537878}
-}, {
-  "serial": str(3).zfill(2),
-  "type": "sensor",
-  "ancestor": "proximity",
-  "position":{
-    "x": 47.410847,
-    "y":  8.537878}
-}, {
-  "serial": str(4).zfill(2),
-  "type": "sensor",
-  "ancestor": "noise",
-  "position":{
-    "x": 46.410847,
-    "y":  7.537178}
-}, {
-  "serial": str(5).zfill(2),
-  "type": "sensor",
-  "ancestor": "distance",
-  "position":{
-    "x": 47.410847,
-    "y":  8.537878}
-}, {
-  "serial": str(6).zfill(2),
-  "type": "sensor",
-  "ancestor": "motion",
-  "position":{
-    "x": 47.410847,
-    "y":  8.537878}
-}, {
-  "serial": str(7).zfill(2),
-  "type": "sensor",
-  "ancestor": "humidity",
-  "position":{
-    "x": 37.410847,
-    "y":  10.137878}
-},{
-        "serial": str(8).zfill(2),
-        "type": "sensor",
-        "ancestor": "humidity",
-        "position": {
-            "x": 37.410847,
-            "y": 10.137878}
-    }, {
-        "serial": str(9).zfill(2),
-        "type": "sensor",
-        "ancestor": "airquality",
-        "position": {
-            "x": 37.410847,
-            "y": 10.137878}
-    }
-]"""
 
 for new_element in list_of_sensors:
     sensor_parser.add_sensor(new_element)
 
-"""
-for id in range(5):
-    new_sensors.append(TemperatureSensor(str(counter).zfill(6), env))
-    counter += 1
-
-for id in range(5):
-    new_sensors.append(HumiditySensor(str(counter).zfill(6), env))
-    counter += 1
-
-for id in range(5):
-    new_sensors.append(NoiseSensor(str(counter).zfill(6), env))
-    counter += 1
-
-for id in range(5):
-    new_sensors.append(MotionSensor(str(counter).zfill(6), env))
-    counter += 1
-
-for id in range(10):
-    new_sensors.append(BrightnessSensor(str(counter).zfill(6), env))
-    counter += 1
-
-
-for id in range(1):
-    new_sensors.append(Antenna(str(counter).zfill(6), env))
-    counter += 1"""
 
 clock = EnvironmentManager(env)
 clock.start()
